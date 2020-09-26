d96649de3cc4ab32ca168e95539f33df12f315cb
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-09-25 21:53:30 -0600

    This was the worst.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,29 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.head = 0
+        self.tail = 1
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        if len(self.storage) == self.capacity:
+            self.storage.pop(self.head)
+            self.storage.insert(self.tail, item)
+            self.head += 1
+            self.tail += 1
 
     def get(self):
-        pass
+        return self.storage
+
+
+dog = RingBuffer(5)
+dog.append(1)
+dog.append(2)
+dog.append(3)
+dog.append(4)
+dog.append(5)
+dog.append(6)
+dog.append(7)
+print(dog.get())
